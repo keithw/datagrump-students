69df7119eebff8dd7464f49444e57d64d7e2ec63
Ubuntu <ubuntu@ip-10-147-177-147.ec2.internal>
2013-03-19 01:39:06 +0000

    Subclassed sender template (Not compiled)

--- datagrump-sender.cc
+++ datagrump-sender.cc
@@ -16,8 +16,9 @@ int main( int argc, char *argv[] )
   bool debug = false;
 	unsigned int init_cwnd = 1;
 	double ai_coeff = 0, md_coeff = 0;
+  double delay_threshold = 100;
 	ControllerType controllerType = CONSTCWND;
-
+ 
   if ( argc == 4 && string( argv[ 3 ] ) == "debug" ) {
     debug = true;
   } else if ( argc == 3 ) {
@@ -26,10 +27,15 @@ int main( int argc, char *argv[] )
 		if( string( argv[ 4 ]) == "constcwnd" && argc == 6){
 			init_cwnd = strtoul(argv[5], NULL, 0);
 			controllerType = CONSTCWND;
-		} else if ( string( argv[ 4 ]) == "aimd" && argc == 7){
-			ai_coeff = strtod(argv[5], NULL);
-			md_coeff = strtod(argv[6], NULL);
+		} else if ( string( argv[ 4 ]) == "aimd" && argc == 8){
+      init_cwnd = strtoul(argv[5], NULL);
+			ai_coeff = strtod(argv[6], NULL);
+			md_coeff = strtod(argv[7], NULL);
 			controllerType = AIMD;
+		} else if ( string( argv[ 4 ]) == "delay" && argc == 7){
+      init_cwnd = strtoul(argv[5], NULL);
+			delay_threshold = strtod(argv[6], NULL);
+			controllerType = DELAY;
 		} else {
     	fprintf( stderr, "Usage: %s IP PORT [debug] [controller]\n", argv[ 0 ] );
     	fprintf( stderr, "Controller Unkown. Available Controllers:\n");
@@ -57,27 +63,31 @@ int main( int argc, char *argv[] )
     uint64_t next_ack_expected = 0;
 
     /* Initialize flow controller */
-   	Controller controller( debug, init_cwnd, controllerType );
-		if(controllerType == AIMD) {
-			controller.setup_aimd(ai_coeff, md_coeff);
-		}
+    Controller * controller;
+		if(controllerType == CONSTCWND) {
+   	  controller = new Controller( debug, init_cwnd);
+		} else if(controllerType == AIMD) {
+   	  controller = new AIMDController( debug, init_cwnd, ai_coeff, md_coeff);
+		} else if(controllerType == DELAY) {
+   	  controller = new DelayController( debug, init_cwnd, delay_threshold);
+		} 
 
     /* Loop */
     while ( 1 ) {
 			/* Ask controller: what is the window size? */
-			unsigned int window_size = controller.window_size();
+			unsigned int window_size = controller->window_size();
 			
 			/* fill up window */
 			while ( sequence_number - next_ack_expected < window_size ) {
 				Packet x( destination, sequence_number++ );
 				sock.send( x );
-				controller.packet_was_sent( x.sequence_number(),
+				controller->packet_was_sent( x.sequence_number(),
 				x.send_timestamp() );
 			}
 			
 			/* Wait for acknowledgement or timeout */
 			struct pollfd fd = { sock.fd(), POLLIN, 0 };
-			int packet_received = poll( &fd, 1, controller.timeout_ms() );
+			int packet_received = poll( &fd, 1, controller->timeout_ms() );
 	    if ( packet_received < 0 ) { /* error */
 				perror( "poll" );
 				throw string( "poll returned error." );
@@ -85,7 +95,7 @@ int main( int argc, char *argv[] )
 				/* send a packet */
 				Packet x( destination, sequence_number++ );
 				sock.send( x );
-				controller.packet_was_sent( x.sequence_number(),
+				controller->packet_was_sent( x.sequence_number(),
 							    x.send_timestamp() );
 			} else {
 				/* we got an acknowledgment */
@@ -96,7 +106,7 @@ int main( int argc, char *argv[] )
 							 ack.ack_sequence_number() + 1 );
 			
 				/* tell the controller */
-				controller.ack_received( ack.ack_sequence_number(),
+				controller->ack_received( ack.ack_sequence_number(),
 				ack.ack_send_timestamp(),
 				ack.ack_recv_timestamp(),
 				ack.recv_timestamp() );
