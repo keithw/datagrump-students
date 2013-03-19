c8ae5f812c352cc057eb64ba2f143a9c8b69dd19
Ubuntu <ubuntu@ip-10-152-168-146.ec2.internal>
2013-03-19 22:55:38 +0000

    trying to react to average rtt.

--- controller.cc
+++ controller.cc
@@ -3,6 +3,9 @@
 #include "controller.hh"
 #include "timestamp.hh"
 
+#include <iostream>
+#include <deque>
+
 using namespace Network;
 
 /* Default constructor */
@@ -11,7 +14,11 @@ Controller::Controller( const bool debug )
 {
 }
 
-int the_window_size = 5;
+int the_window_size = 10;
+
+std::deque<uint64_t> packet_times;
+std::deque<uint64_t> time_diffs;
+
 /* Get current window size, in packets */
 unsigned int Controller::window_size( void )
 {
@@ -25,25 +32,27 @@ unsigned int Controller::window_size( void )
   return the_window_size;
 }
 
-uint64_t prev_sequence_number = 0;
+uint64_t last_ack_seq_num = 0;
 /* A packet was sent */
 void Controller::packet_was_sent( const uint64_t sequence_number,
 				  /* of the sent packet */
 				  const uint64_t send_timestamp )
                                   /* in milliseconds */
 {
+  packet_times.push_back(send_timestamp);
   /* Default: take no action */
-  if (sequence_number <= prev_sequence_number) {
+
+
+  /*  if (sequence_number <= last_ack_seq_num) {
     // retransmission due to timeout, packetloss, drop?
     // i.e. detect tcp cong., need to decrease cwnd by multiplicative factor
-    the_window_size /= 2; //TCP_MULTI_DEC_FACTOR;
+    the_window_size /= 2;
     fprintf(stdout, "Timeout and retransmitting packet with seq_num %lu \n", sequence_number);
 
   } else {
-    // normal transmission, do nothing? will additive increase on acks
-    // update latest sequence number
-    // WRONG!!???    prev_sequence_number = sequence_number;
+    // normal transmission
   }
+  */
 
   if ( debug_ ) {
     fprintf( stderr, "At time %lu, sent packet %lu.\n",
@@ -62,16 +71,41 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
 			       const uint64_t timestamp_ack_received )
                                /* when the ack was received (by sender) */
 {
-  /* Default: take no action */
+  uint64_t diff = timestamp_ack_received - packet_times.front();
+  time_diffs.push_front(diff);
+  packet_times.pop_front();
+
+  // loop through first 10 diffs to get average RTT
+  uint64_t avg_rtt = 0;
+
+  fprintf(stdout, "diff: %lu , time_diffs[0]: %lu , cwnd: %i \n", diff, time_diffs[0], the_window_size);
+
+  if(time_diffs.size() >= 10) {
+  for(int i=0; i<10; i++) {
+    avg_rtt += time_diffs[i];
+  }
+  avg_rtt /= 10;
+  } else {    
+    //
+  }
+
+  if (avg_rtt > 100) {
+    // multiplicative decrease
+    the_window_size /= 2;
+  } else {
+
   // ACK received, do additive increase
   // increment by 1/cwnd, use cwnd_fraction to keep track of fractional increment
   // of cwnd, until add up to one cwnd size.
-  cwnd_fraction++;
-  if (cwnd_fraction >= the_window_size) {
-    cwnd_fraction = 0;
-    the_window_size += 1; //TCP_ADD_INC_FACTOR;
+    cwnd_fraction++;
+    if (cwnd_fraction >= the_window_size) {
+      cwnd_fraction = 0;
+      the_window_size += 1;
+    }
   }
-  prev_sequence_number = sequence_number_acked+1;
+
+
+  last_ack_seq_num = sequence_number_acked+1; // remove?
   
   if ( debug_ ) {
     fprintf( stderr, "At time %lu, received ACK for packet %lu",
