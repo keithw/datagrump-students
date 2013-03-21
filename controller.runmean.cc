1dffd6538f3b01ed3d0e7f08072a213d4ef65e02
thashim <thashim@mit.edu>
2013-03-20 23:17:24 -0400

    Added initial runmean stuff

--- controller.runmean.cc
+++ controller.runmean.cc
@@ -38,8 +38,7 @@ Controller::Controller( const bool debug )
 void Controller::estimateParameters() {
 }
 
-void Controller::refineParameters() {
-}
+
 
 int Controller::chompWindow(int cint) {
   return cint;
@@ -49,7 +48,6 @@ int Controller::chompWindow(int cint) {
 unsigned int Controller::window_size( void )
 {
   estimateParameters();
-  refineParameters();
   /* Default: fixed window size of one outstanding packet */
   int cint = (int)cwind;
 
@@ -86,6 +84,24 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
                                const uint64_t timestamp_ack_received )
 /* when the ack was received (by sender) */
 {
+  refineParameters(sequence_number_acked,send_timestamp_acked,recv_timestamp_acked,timestamp_ack_received);
+  if ( debug_ ) {
+    fprintf( stderr, "At time %lu, received ACK for packet %lu",
+             timestamp_ack_received, sequence_number_acked );
+
+    fprintf( stderr, " (sent %lu, received %lu by receiver's clock).\n",
+             send_timestamp_acked, recv_timestamp_acked );
+  }
+}
+
+void Controller::refineParameters(const uint64_t sequence_number_acked,
+                               /* what sequence number was acknowledged */
+                               const uint64_t send_timestamp_acked,
+                               /* when the acknowledged packet was sent */
+                               const uint64_t recv_timestamp_acked,
+                               /* when the acknowledged packet was received */
+                               const uint64_t timestamp_ack_received )
+{
   runmean.push(send_timestamp_acked);
 
   while(runmean.size()>0 && (timestamp_ack_received-runmean.front())>(resolution+rtt)){
@@ -95,13 +111,6 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
   }
   fprintf(stderr, "size: %i\n",(int)runmean.size());
   cwind=((double)runmean.size())/resolution*rtt;
-  if ( debug_ ) {
-    fprintf( stderr, "At time %lu, received ACK for packet %lu",
-             timestamp_ack_received, sequence_number_acked );
-
-    fprintf( stderr, " (sent %lu, received %lu by receiver's clock).\n",
-             send_timestamp_acked, recv_timestamp_acked );
-  }
 }
 
 /* How long to wait if there are no acks before sending one more packet */
