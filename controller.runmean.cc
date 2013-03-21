2454ccaf6b88c15d9e1424a899209a93bfbd87e8
thashim <thashim@mit.edu>
2013-03-21 02:31:04 -0400

    Fixed sign error

--- controller.runmean.cc
+++ controller.runmean.cc
@@ -107,7 +107,7 @@ void Controller::refineParameters(const uint64_t sequence_number_acked,
     runmean.pop();
   }
   fprintf(stderr, "size: %i\n",(int)runmean.size());
-  if(cwind <= runmean.size()/resolution*rtt){
+  if(cwind >= runmean.size()/resolution*rtt){
     cwind= runmean.size()/resolution*rtt;
   }else{
     cwind=((double)runmean.size())/resolution*rtt*0.914+0.05811*rtt;
