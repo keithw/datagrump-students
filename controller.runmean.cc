4b568ac1df35f0c8572bf1812b5bd144edbb57c6
Paresh Malalur <pareshmg@mit.edu>
2013-03-21 17:48:44 -0400

    monitor bursts

--- controller.runmean.cc
+++ controller.runmean.cc
@@ -113,7 +113,7 @@ double Controller::estimateParameters() {
     // if we are getting acks faster => network has recoved and queue is
     // being flushed and we are getting fast responses
     double wt = 0.5;
-    if (rho > 0.25) wt = 0.9;
+    //if (rho > 0.25) wt = 0.9;
     fprintf(fsend, "%lu: cwinds: %.4f, %.4f : %.4f\n", tStamp, cwindDL, cwind, ackTracker);
     cwind = (wt*cwindDL + (1-wt)*cwind);
   } else {
@@ -145,7 +145,7 @@ int Controller::chompWindow(int cint, double cwindDL) {
   if (cint < 1)
     if ((lastCW == 0) || (ackTracker == 0.0))
       cint = 1;
-  if (rho > 0.25) cint += 5;
+  //if (rho > 0.25) cint += 1;
   // if ((lastCW >= cint) && (lastCW <= lastPB) && (lastCW <= 1.5*(cint)))
   //   cint = lastCW+1;
 
