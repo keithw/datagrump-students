1d894a376e5807b21e9d4339f6f05bfe46eee2e5
thashim <thashim@mit.edu>
2013-03-21 02:57:57 -0400

    using constant truncated poisson approx

--- controller.runmean.cc
+++ controller.runmean.cc
@@ -107,12 +107,11 @@ void Controller::refineParameters(const uint64_t sequence_number_acked,
     runmean.pop();
   }
   fprintf(stderr, "size: %i\n",(int)runmean.size());
-  if(cwind >= runmean.size()/resolution*rtt-5){
+  if(cwind > runmean.size()/resolution*rtt-5){
     cwind= runmean.size()/resolution*rtt+5;
   }else{
-    cwind=((double)runmean.size())/resolution*rtt*1.2+0.05811*rtt;
+    cwind+=0.65;
   }
-  
   if ( debug_ ) {
     fprintf( stderr, "At time %lu, received ACK for packet %lu",
              timestamp_ack_received, sequence_number_acked );
