dceef4e9c4eeb83b24b188eae4a9693e33bd9d9e
thashim <thashim@mit.edu>
2013-03-21 14:45:54 -0400

    Escape cases

--- controller.runmean.cc
+++ controller.runmean.cc
@@ -163,12 +163,16 @@ void Controller::refineParameters(const uint64_t sequence_number_acked,
   double icept = -1.0402;
   double tfbest = 2*sqrt(runmean.size()+3/8)*slope+icept;
   double bwest=(tfbest*tfbest/4-1/8)/20;
+  if(mrtt > (rtt/2+20)){
+    cwind= bwet*(rtt-10);
+  }else{
   if(mrtt > (rtt/2+5)){
   //if(cwind > runmean.size()/resolution*rtt){
     cwind= bwest*(rtt+20);
   }else{
-    cwind= (bwest+sqrt(bwest)*0.598+1.11023)*(rtt+20);
-  }
+    cwind= (bwest+sqrt(bwest)*0.598+1.11023)*(rtt);
+    //cwind= bwest*(rtt+20);
+  }}
   if ( debug_ ) {
     fprintf( stderr, "At time %lu, received ACK for packet %lu",
              timestamp_ack_received, sequence_number_acked );
