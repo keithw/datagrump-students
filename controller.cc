9591f8764db87d5ade35a8036968776fab8d089c
Omid Aryan <omidmi6@gmail.com>
2013-03-21 16:05:43 -0700

    no message

--- controller.cc
+++ controller.cc
@@ -96,11 +96,11 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
   rttdev = (beta*dev) + ((1-beta)*rttdev);
   //timeout_float = srtt + (4*rttdev);
   
-  uint64_t avg = 0;
-  for (int n=0 ; n<rsize ; n++ ){
-  	avg = avg + rtt_rec[n];
-  }
-  avg = avg/rsize;
+  // uint64_t avg = 0;
+//   for (int n=0 ; n<rsize ; n++ ){
+//   	avg = avg + rtt_rec[n];
+//   }
+//   avg = avg/rsize;
 //   
 //   if (rtt > (avg)){
 //   	window_float = window_float + (1.0/window);
@@ -109,10 +109,10 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
 //   	window_float = window_float - (0.5/window);
 //   }
 //   
-  for (int n=0 ; n<(rsize-1) ; n++ ){
-  	rtt_rec[n] = rtt_rec[n+1];
-  }
-  rtt_rec[rsize-1] = rtt;
+  // for (int n=0 ; n<(rsize-1) ; n++ ){
+//   	rtt_rec[n] = rtt_rec[n+1];
+//   }
+//   rtt_rec[rsize-1] = rtt;
   
 }
 
