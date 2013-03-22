2a1435ea1bc23a65de16a9db6f683d280c017358
Paresh Malalur <pareshmg@mit.edu>
2013-03-22 01:06:12 -0400

    merged with master

--- controller.runmean.cc
+++ controller.runmean.cc
@@ -52,9 +52,9 @@ Controller::Controller( const bool debug )
 /* Get current window size, in packets */
 unsigned int Controller::window_size( void )
 {
-  double cwindDL = estimateParameters();
+  //double cwindDL = estimateParameters();
   int cint = (int) cwind;
-  cint = chompWindow(cint, cwindDL);
+  cint = chompWindow(cint, cwind);
   if(cint<1){cint=1;}
   if ( debug_ ) {
     fprintf( stderr, "At time %lu, return window_size = %d.\n",
