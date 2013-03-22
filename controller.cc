147f7fd2c45423b8e209c1f8705a090c307d9b90
Colleen Josephson <ubuntu@ip-10-165-5-232.ec2.internal>
2013-03-22 01:51:59 +0000

    broken AIMD

--- controller.cc
+++ controller.cc
@@ -1,9 +1,12 @@
 #include <stdio.h>
-
+#include <map>
 #include "controller.hh"
 #include "timestamp.hh"
 
 using namespace Network;
+std::map<uint64_t,uint64_t> unacked;
+std::map<uint64_t, uint64_t>::iterator it;
+float ws = 1.0;
 
 /* Default constructor */
 Controller::Controller( const bool debug )
@@ -15,7 +18,19 @@ Controller::Controller( const bool debug )
 unsigned int Controller::window_size( void )
 {
   /* Default: fixed window size of one outstanding packet */
-  int the_window_size = 16;
+  //makes sure window s
+  int the_window_size = ((int)ws >= 1 ? (int) ws: 1);
+  
+  //make sure none of the outstanding packets have timed out.
+  uint64_t delta;
+  for(it=unacked.begin(); it != unacked.end(); it++) {
+    delta = timestamp() - (it->second);
+    if (delta > timeout_ms()){
+      ws = ws/2.0; //halve window size
+      fprintf( stderr, "Pkt number %lu timed out\n", (it->first));
+      unacked.erase(it->first);
+    }
+  }
 
   if ( debug_ ) {
     fprintf( stderr, "At time %lu, return window_size = %d.\n",
@@ -31,7 +46,10 @@ void Controller::packet_was_sent( const uint64_t sequence_number,
 				  const uint64_t send_timestamp )
                                   /* in milliseconds */
 {
-  /* Default: take no action */
+
+  //add sent pkt to dict
+  unacked[sequence_number] = send_timestamp;
+
   if ( debug_ ) {
     fprintf( stderr, "At time %lu, sent packet %lu.\n",
 	     send_timestamp, sequence_number );
@@ -48,7 +66,15 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
 			       const uint64_t timestamp_ack_received )
                                /* when the ack was received (by sender) */
 {
-  /* Default: take no action */
+
+  //remove received packet from map
+  if(unacked.find(sequence_number_acked) != unacked.end()){
+    unacked.erase(sequence_number_acked);//erase the entry, if it existed (might not after timeout)
+    ws+=(2.0/ws);
+    else fprintf(stderr, "Ack not found in outstanding\n");
+  } else fprintf(stderr, "Ack not found in outstanding\n");
+  
+  
 
   if ( debug_ ) {
     fprintf( stderr, "At time %lu, received ACK for packet %lu",
