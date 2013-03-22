5593b3ccdf39e9961d0a04a961ae54871bb4134a
thashim <thashim@mit.edu>
2013-03-21 22:14:27 -0400

    smaller window size (100->80)

--- controller.runmean.cc
+++ controller.runmean.cc
@@ -24,7 +24,7 @@ Controller::Controller( const bool debug )
     cwind(10),
     runmean(std::queue<int>()),
     packetBalance(std::list<uint64_t>()),
-    resolution(100),
+    resolution(80),
     rtt(40),
     rttsum(400),
     rttn(10),
