27bd8c62adc701617ce254b4a0d637c1f1904747
Omid Aryan <omidmi6@gmail.com>
2013-03-22 11:08:46 -0700

    no message

--- controller.cc
+++ controller.cc
@@ -10,7 +10,7 @@ using namespace Network;
 Controller::Controller( const bool debug )
   : debug_( debug ), window(40), window_float(40.0), timeout(1500),
   rtt(0), srtt(0), alpha(0.4), dev(0), rttdev(0),
-  beta(0.4), rtt_rec{0,0}, rsize(sizeof(rtt_rec)/sizeof(float)),
+  beta(0.4), rtt_rec{0,0,0}, rsize(sizeof(rtt_rec)/sizeof(float)),
   avg(0)
 { 
 }
@@ -93,11 +93,11 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
   //int rtt_i = (int) rtt;
   if (rtt < (avg)){
   	//window_float = (1.0+(1.0*(avg_i-rtt_i)/avg_i))*window_float;// + (1.0/window);
-  	window_float = window_float + (4.0/window_float);
+  	window_float = window_float + (4.5/window_float);
   }
   else{
   	//window_float = (1.0*(rtt_i-avg_i)/avg_i)*window_float;// - (1.5/window);
-  	  	window_float = window_float - (2.0/window_float);
+  	  	window_float = window_float - (3.0/window_float);
   }
   
   for (int n=0 ; n<(rsize-1) ; n++ ){
