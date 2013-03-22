5c9ce39b7636dec8a7d1740bad11376edd3bcaed
thashim <thashim@mit.edu>
2013-03-21 20:53:13 -0400

    Upping spike size

--- controller.runmean.cc
+++ controller.runmean.cc
@@ -135,8 +135,8 @@ void Controller::refineParameters(const uint64_t sequence_number_acked,
     double mrtt=diffsum/((int)rtimes.size());
     //fprintf(stderr,"rttmean: %i\n",(int)mrtt);
     // if our RTT is low and stable with at least 2xRTT our last time
-    if(mrtt< (rtt/2+rtteps/2) && ((timestamp_ack_received-lastspike)>(rtt))){
-      cwind=bwest*(rtt+2*rtteps);
+    if(mrtt< (rtt/2+rtteps/2) && ((timestamp_ack_received-lastspike)>2*(rtt))){
+      cwind=bwest*(rtt+4*rtteps);
       lastspike=timestamp_ack_received;
       fprintf(stdout,"%i,%i,%i,%.4f,%.4f,TRUE,%.4f\n",
 	      (int)(timestamp_ack_received-start_time),
