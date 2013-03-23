257d2e1c9d6a84c9f491c8065d5be1ad14fb3dde
Omid Aryan <omidmi6@gmail.com>
2013-03-22 18:05:47 -0700

    no message

--- controller.cc
+++ controller.cc
@@ -94,8 +94,8 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
   if (rtt < (avg)){
   	//window_float = (1.0+(1.0*(avg_i-rtt_i)/avg_i))*window_float;// + (1.0/window);
   	//window_float = window_float + (4.75/window_float);
-  	//window_float = window_float + (4.0/window_float);
-  	window_float = 1.05*window_float;
+  	window_float = window_float + (8.0/window_float);
+  	//window_float = 1.05*window_float;
   }
   else{
   	//window_float = (1.0*(rtt_i-avg_i)/avg_i)*window_float;// - (1.5/window);
