215b489f84de57a01d1ddfc3094e756c9952e09d
Omid Aryan <omidmi6@gmail.com>
2013-03-22 17:19:17 -0700

    no message

--- controller.cc
+++ controller.cc
@@ -8,7 +8,7 @@ using namespace Network;
 
 /* Default constructor */
 Controller::Controller( const bool debug )
-  : debug_( debug ), window(40), window_float(40.0), timeout(1500),
+  : debug_( debug ), window(30), window_float(30.0), timeout(1500),
   rtt(0), srtt(0), alpha(0.4), dev(0), rttdev(0),
   beta(0.4), rtt_rec{0,0,0}, rsize(sizeof(rtt_rec)/sizeof(float)),
   avg(0), ratio(0), wb(2)
